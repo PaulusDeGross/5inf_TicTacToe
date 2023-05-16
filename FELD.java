bf2bcc1dcaadbedaed643d53b6f34e5d2232a8fc
GasmaskskeletonGames2 <132997946+GasmaskskeletonGames2@users.noreply.github.com>
2023-05-16 11:23:41 +0200

    Add files via upload

--- FELD.java
+++ FELD.java
@@ -1,7 +1,30 @@
 public class FELD {
+    // Attribute
+    private int x;
+    private int y;
+    private ANZEIGE anzeige;
 
-    public FELD(){
-        System.out.println("FELD");
+    // Konstruktor
+    public FELD ( int x_neu, int y_neu, ANZEIGE anzeige_neu )
+    {
+        x = x_neu;
+        y = y_neu;
+        anzeige = anzeige_neu;
     }
 
+    // Methoden
+    public void Anzeigen()
+    {
+        anzeige.Anzeigen( x, y );
+    }
+
+    public void Entfernen()
+    {
+        anzeige.Loeschen( x, y );
+    }
+
+    public boolean IstGetroffen( int x_vgl, int y_vgl )
+    {
+        return x == x_vgl && y == y_vgl;
+    }
 }
--- FELDLISTE.java
+++ FELDLISTE.java
@@ -1,7 +1,74 @@
 public class FELDLISTE {
 
-    public FELDLISTE(){
-        System.out.println("FELDLISTE");
+     private FELD[] felder;
+    private int anzahl;
+
+    // Konstruktor
+    public FELDLISTE ()
+    {
+        felder = new FELD[KONST.spielfeldgroesse * KONST.spielfeldgroesse];
+        anzahl = 0;
+    }
+
+    // Methoden
+    public void Hinzufuegen( FELD f )
+    {
+        if( anzahl < felder.length )
+        {
+            felder[ anzahl ] = f;
+            f.Anzeigen();
+            anzahl = anzahl + 1;
+        }
+    }
+
+    public void Entfernen( int nr )
+    {
+        if( nr >= 0 && nr < anzahl )
+        {
+            felder[ nr ].Entfernen();
+            anzahl = anzahl - 1;
+            int i = nr;
+            while( i < anzahl )
+            {
+                felder[ i ] = felder[ i + 1 ];
+                i = i + 1;
+            }
+            felder[ anzahl ] = null;
+        }
+    }
+
+    public int Suchen( int x, int y )
+    {
+        int i = 0;
+        while( i < anzahl )
+        {
+            if( felder[ i ].IstGetroffen( x, y ) )
+            {
+                return i;
+            }
+            i = i + 1;
+        }
+        return -1;
+    }
+
+    public boolean IstGetroffen( int x, int y )
+    {
+        return Suchen( x, y ) > -1;
+    }
+
+    public void LetztenEntfernen()
+    {
+        Entfernen( 0 );
+    }
+
+    public void Entfernen( int x, int y )
+    {
+        Entfernen( Suchen( x, y ) );
+    }
+
+    public int AnzahlGeben()
+    {
+        return anzahl;
     }
 
 }
