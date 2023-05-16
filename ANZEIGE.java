6ecbaf940aa5096d2c1d41cedd6ae6cce1aa517c
GasmaskskeletonGames2 <132997946+GasmaskskeletonGames2@users.noreply.github.com>
2023-05-16 11:20:48 +0200

    Add files via upload

--- ANZEIGE.java
+++ ANZEIGE.java
@@ -1,7 +1,41 @@
-public class ANZEIGE {
 
-    public ANZEIGE(){
-        System.out.println("ANZEIGE");
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class ANZEIGE 
+{
+     protected Graphics leinwand;
+
+    // Konstruktor
+    public ANZEIGE ( Graphics leinwand )
+    {
+        this.leinwand = leinwand;
+    }
+    
+    // Methoden
+    private int XInPixel( int x )
+    {
+        return 15 + 25 * x;
     }
 
+    private int YInPixel( int y )
+    {
+        return 65 + 25 * y;
+    }
+
+    public void Anzeigen( int x, int y )
+    {
+        Zeichnen( XInPixel( x ), YInPixel( y ), 25, 25 );
+    }
+
+    public void Loeschen( int x, int y )
+    {
+        leinwand.setColor( Color.WHITE );
+        leinwand.fillRect( XInPixel( x ), YInPixel( y ), 25, 25 );
+    }
+
+    private void Zeichnen( int x, int y, int b, int h )
+    {
+        //FelderZeichnen
+    }
 }
