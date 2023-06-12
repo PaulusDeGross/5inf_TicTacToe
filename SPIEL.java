191980f0695f1b532ed7d9340f4c05f6c27e281d
PaulusDeGross <65727558+PaulusDeGross@users.noreply.github.com>
2023-06-12 09:00:14 +0200

    Update SPIEL.java

--- SPIEL.java
+++ SPIEL.java
@@ -1,4 +1,5 @@
 
+
 public class SPIEL{
 
     private FENSTER fenster;
@@ -30,7 +31,6 @@ public class SPIEL{
         int feld = feldliste.Suchen(x, y);
         System.out.println("Feld: " + feld + "| Spieler: " + spieler);
         FeldSetzen(feld);
-        SpielerWechseln();
         fenster.repaint();
     }
 
@@ -49,20 +49,50 @@ public class SPIEL{
         if(feldliste.SymbolGeben(nr) == SYMBOLE.LEER && spieler == SYMBOLE.X)
         {
             feldliste.XSetzen(nr);
+            kreuzanzeige.Zeichnen(feldliste.XGeben(nr), feldliste.YGeben(nr), KONST.feldgroesse, KONST.feldgroesse);
+            fenster.repaint();
+            SpielerWechseln();
         }
-        else if(feldliste.SymbolGeben(nr) == SYMBOLE.LEER && spieler == SYMBOLE.X)
+        else if(feldliste.SymbolGeben(nr) == SYMBOLE.LEER && spieler == SYMBOLE.O)
         {
-             feldliste.OSetzen(nr);
+            feldliste.OSetzen(nr);
+            kreisanzeige.Zeichnen(feldliste.XGeben(nr), feldliste.YGeben(nr), KONST.feldgroesse, KONST.feldgroesse);
+            fenster.repaint();
+            SpielerWechseln();
         }
         else
         {
             System.out.println("Feld ist besetzt");
         }
+        SpielEvaluieren();
     }
 
-    private void SpielEvaluieren()
+    private SYMBOLE SpielEvaluieren()
     {
-        
+        for(SYMBOLE symbol : SYMBOLE.values())
+        {
+            if(symbol == SYMBOLE.LEER)
+            {
+                continue;
+            }
+            if(
+                feldliste.SymbolGeben(0) == symbol && feldliste.SymbolGeben(1) == symbol && feldliste.SymbolGeben(2) == symbol ||
+                feldliste.SymbolGeben(3) == symbol && feldliste.SymbolGeben(4) == symbol && feldliste.SymbolGeben(5) == symbol ||
+                feldliste.SymbolGeben(6) == symbol && feldliste.SymbolGeben(7) == symbol && feldliste.SymbolGeben(8) == symbol ||
+                
+                feldliste.SymbolGeben(0) == symbol && feldliste.SymbolGeben(3) == symbol && feldliste.SymbolGeben(6) == symbol ||
+                feldliste.SymbolGeben(1) == symbol && feldliste.SymbolGeben(4) == symbol && feldliste.SymbolGeben(7) == symbol ||
+                feldliste.SymbolGeben(2) == symbol && feldliste.SymbolGeben(5) == symbol && feldliste.SymbolGeben(8) == symbol ||
+                
+                feldliste.SymbolGeben(0) == symbol && feldliste.SymbolGeben(4) == symbol && feldliste.SymbolGeben(8) == symbol ||
+                feldliste.SymbolGeben(2) == symbol && feldliste.SymbolGeben(4) == symbol && feldliste.SymbolGeben(6) == symbol
+            )
+            {
+                System.out.println(symbol + " hat gewonnen.");
+            }
+            return symbol;
+        }
+        return null;
     }
     
     public FELDANZEIGE FeldanzeigeGeben()
@@ -71,3 +101,4 @@ public class SPIEL{
     }
     
 }
+
