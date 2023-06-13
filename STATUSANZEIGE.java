ce2ee8ee1a8cc20cfbb1a0b816c88d28ff3d2a2d
GasmaskskeletonGames2 <132997946+GasmaskskeletonGames2@users.noreply.github.com>
2023-06-13 12:12:41 +0200

    Add files via upload

---
+++ STATUSANZEIGE.java
@@ -0,0 +1,37 @@
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+
+public class STATUSANZEIGE extends ANZEIGE
+{
+    // Attribute
+
+    // Konstruktor
+    public STATUSANZEIGE (Graphics leinwand)
+    {
+        super(leinwand);
+    }
+
+    // Methoden
+     @Override
+    public void Zeichnen(int x, int y, int b, int h)
+    {
+        //nichts tun
+        //später mal noch neue Klasse spielfeldAnzeige machen und da Zeichnen reintun
+    }
+    
+    // Methoden
+    public void Anzeigen( int x, int y, String text, int fontgroesse )
+    {
+        leinwand.setColor( Color.BLACK );
+        leinwand.setFont( new Font( "Arial", Font.PLAIN, fontgroesse ) );
+        leinwand.drawString( text, x, y );
+    }
+
+    public void Loeschen(int b, int h)
+    {
+        leinwand.setColor( Color.WHITE );
+        leinwand.fillRect( 0, 0, b, h);
+    }
+}
