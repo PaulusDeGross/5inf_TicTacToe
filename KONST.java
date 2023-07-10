3cde63749894b12597d51b6e2774f39e70946527
GasmaskskeletonGames2 <132997946+GasmaskskeletonGames2@users.noreply.github.com>
2023-07-10 08:41:45 +0200

    Add files via upload

--- KONST.java
+++ KONST.java
@@ -12,16 +12,16 @@ public class KONST {
     private static final int actionbar_offset = 28;
     public static final int feld_y_offset = 0;
     public static final int feld_x_offset = 0;
-    public static final int offset = 100 + actionbar_offset;
+    public static final int offset = 50 + actionbar_offset;
     public static final int fensterbreite = 600;
     public static final int fensterhoehe = fensterbreite + offset;
-    public static final boolean groesseveränderbar = true;
+    public static final boolean groesseveraenderbar = false;
     public static final boolean sichtbar = true;
     public static final Color hintergrundfarbe = Color.WHITE;
     public static final int spielfeldgroesse = 3;
     public static final int startX = 1;
     public static final int startY = 1;
-    public static final int feldgroesse = ((fensterhoehe - offset) / spielfeldgroesse);
+    public static final int feldgroesse = ((fensterhoehe - offset - 60) / spielfeldgroesse);
     public static final int[][][]eckpunkteFelder = eckpunkteFelderGeben(startX, startY);
     public static final SPIEL spiel = new SPIEL();
     
